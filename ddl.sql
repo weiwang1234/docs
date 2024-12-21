0ecbae5f2c5b5dac91b82d67227e7dca883b498a
ww <106657674+weiwang1234@users.noreply.github.com>
2024-12-22 00:13:26 +0800

    问题记录

--- ddl.sql
+++ ddl.sql
@@ -1,15 +1,11 @@
-CREATE TABLE invmanage.user_info (
-	userid BIGINT auto_increment NOT NULL COMMENT '用户id',
-	username varchar(100) NULL COMMENT '用户名称',
-	Password varchar(100) NULL COMMENT '用户密码',
-	status varchar(100) NULL COMMENT '用户状态',
-	CONSTRAINT user_info_pk PRIMARY KEY (userid)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+-- invmanage.user_info definition
 
-ALTER TABLE invmanage.user_info MODIFY COLUMN status varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '用户状态（1有效，2失效）';
-
-ALTER TABLE invmanage.user_info ADD loginid varchar(100) NULL COMMENT '登录id';
-ALTER TABLE invmanage.user_info CHANGE loginid loginid varchar(100) NULL COMMENT '登录id' AFTER userid;
+CREATE TABLE `user_info` (
+  `userid` bigint NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `loginid` varchar(100) DEFAULT NULL COMMENT '登录id',
+  `username` varchar(100) DEFAULT NULL COMMENT '用户名称',
+  `Password` varchar(100) DEFAULT NULL COMMENT '用户密码',
+  `status` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户状态（1有效，2失效）',
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `user_info_unique` (`loginid`)
+) ;
